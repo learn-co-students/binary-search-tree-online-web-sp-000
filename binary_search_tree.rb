62281bfce185cf024048d7e3e6402766ae6af6de
Cole Rau <colevrau@gmail.com>
2021-01-18 19:15:32 -0800

    everything done except

---
+++ binary_search_tree.rb
@@ -0,0 +1,27 @@
+class BST
+  attr_accessor :data, :left, :right
+
+  def initialize(data)
+    self.data = data
+  end
+
+  def insert(data)
+    if data <= self.data
+      if self.left.nil? 
+        self.left = BST.new(data)
+      else 
+        self.left.insert(data)
+      end 
+    else
+      if self.right.nil? 
+        self.right = BST.new(data)
+      else 
+        self.right.insert(data)
+      end 
+    end 
+  end
+
+  def each 
+    
+  end
+end
