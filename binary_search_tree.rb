92d8bcfe16a236aa2b35fcaeab00429f66813d6d
soncrayon <arnold.curtis@gmail.com>
2021-06-24 22:16:48 -0400

    Done.

--- binary_search_tree.rb
+++ binary_search_tree.rb
@@ -1,6 +1,20 @@
 class BST
-    attr_accessor :data 
-    def initialize (root)
-        @data = root 
+
+    attr_accessor :data, :left, :right 
+
+    def initialize (data)
+        @data = data 
+        @left = nil
+        @right = nil 
     end
+
+    def insert (data)
+        if data <= @data then 
+            @left.nil? ? self.left = BST.new(data) : @left.insert(data)
+        else 
+            @right.nil? ? self.right = BST.new(data) : @right.insert(data)
+        end
+    end
+
+  
 end
